c08d24f1f18dcf8d26357ab5de85407bd80ddc58
Sarah Hong <ssh2198@columbia.edu>
2023-09-20 16:09:18 -0400

    Boxplot

--- standard_deviation.py
+++ standard_deviation.py
@@ -5,7 +5,7 @@ import numpy as np
 def mean(arr=[]):
     total, n = 0, 0
     for i in range(0, len(arr)):
-        total += float(arr[i])
+        total += arr[i]
         n += 1
     return total/n
 
@@ -13,7 +13,7 @@ def sample_variance(arr = []):
     sample_mean = mean(arr)
     sum, n = 0, 0
     for i in range(0, len(arr)):
-        sum = sum + (float(arr[i]) - sample_mean)**2
+        sum = sum + (arr[i] - sample_mean)**2
         n += 1
     return sum / (n-1)
 
@@ -29,10 +29,10 @@ def create_arr_from_str(arr_str: str):
         if arr_str[char].isdigit() is True:
             current = current + arr_str[char]
             if char == len(arr_str)-1:
-                arr.append(current)
+                arr.append(float(current))
                 arr_count += 1
         elif arr_str[char].isdigit() is False and arr_str[char-1].isdigit() is True:
-            arr.append(current)
+            arr.append(float(current))
             arr_count += 1
             current = ""
     return arr
@@ -45,4 +45,6 @@ if __name__ == "__main__":
     print(f"Sample variance of entered array is: {sample_variance(arr)}")
     print(f"Standard deviation of entered array is: {standard_deviation(sample_variance(arr))}")
 
+    plt.boxplot(arr)
+    plt.show()
     
